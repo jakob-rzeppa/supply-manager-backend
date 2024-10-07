6f69b05673e6d931dcf900d433b124e44fac8d47
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-10-07 11:25:04 +0200

    added dotenv and gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT;
+if (!port) throw new Error("PORT must be provided");
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
