43adeb7e32d6cf8691e39eaa2b32e18b89ab2fc1
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-10-29 20:20:50 +0100

    removed NODE_ENV evironment var

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,14 +15,8 @@ if (!ACCESS_TOKEN_SECRET) {
   throw new Error("ACCESS_TOKEN_SECRET must be provided");
 }
 
-const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error("NODE_ENV must be provided");
-}
-
 export const env = {
   PORT,
   MONGO_URI,
   ACCESS_TOKEN_SECRET,
-  NODE_ENV,
 };
