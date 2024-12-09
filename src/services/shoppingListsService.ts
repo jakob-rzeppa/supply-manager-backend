c92d6a89155575d26abce08831c86448bf4b6849
jakob-rzeppa <rzeppa.jakob@gmail.com>
2024-12-09 19:12:59 +0100

    implement shopping lists service methods and add initial test file

--- src/services/shoppingListsService.ts
+++ src/services/shoppingListsService.ts
@@ -1,26 +1,114 @@
+import mongoose from "mongoose";
+import shoppingListsDatabase from "../database/shoppingLists/shoppingListsDatabase";
 import ProductDto from "../dtos/product.dto";
 import ShoppingListDto from "../dtos/shoppingList.dto";
+import { Product } from "../database/product/productDatabase.types";
+import { ShoppingList } from "../database/shoppingLists/shoppingListsDatabase.types";
+import { catchPromiseError } from "../utilityFunctions/errorHandling";
 
 const shoppingListsService = {
   getShoppingLists: async (userId: string): Promise<ShoppingListDto[]> => {
-    return [];
+    const [error, shoppingLists] = await catchPromiseError(
+      shoppingListsDatabase.getShoppingLists(
+        new mongoose.Types.ObjectId(userId)
+      )
+    );
+    if (error) {
+      throw error;
+    }
+
+    const shoppingListsDto = shoppingLists.map((shoppingList: ShoppingList) => {
+      return {
+        id: shoppingList._id.toString(),
+        user_id: shoppingList.user_id.toString(),
+        name: shoppingList.name,
+        description: shoppingList.description,
+        products: shoppingList.products.map(
+          (product: { product: Product; amount: number }) => {
+            return {
+              product: {
+                id: product.product._id.toString(),
+                ean: product.product.ean,
+                user_id: product.product.user_id.toString(),
+                name: product.product.name,
+                description: product.product.description,
+                items: product.product.items,
+              },
+              amount: product.amount,
+            };
+          }
+        ),
+      };
+    });
+
+    return shoppingListsDto;
   },
   getShoppingListById: async (
     id: string,
     userId: string
   ): Promise<ShoppingListDto> => {
-    return {} as ShoppingListDto;
+    const [error, shoppingList] = await catchPromiseError(
+      shoppingListsDatabase.getShoppingListById(
+        new mongoose.Types.ObjectId(id),
+        new mongoose.Types.ObjectId(userId)
+      )
+    );
+    if (error) {
+      throw error;
+    }
+
+    return {
+      id: shoppingList._id.toString(),
+      user_id: shoppingList.user_id.toString(),
+      name: shoppingList.name,
+      description: shoppingList.description,
+      products: shoppingList.products.map(
+        (product: { product: Product; amount: number }) => {
+          return {
+            product: {
+              id: product.product._id.toString(),
+              ean: product.product.ean,
+              user_id: product.product.user_id.toString(),
+              name: product.product.name,
+              description: product.product.description,
+              items: product.product.items,
+            },
+            amount: product.amount,
+          };
+        }
+      ),
+    };
   },
   createShoppingList: async (
     userId: string,
     shoppingListInfo: Omit<ShoppingListDto, "id" | "products" | "user_id">
   ): Promise<ShoppingListDto> => {
-    return {} as ShoppingListDto;
+    const [error, shoppingList] = await catchPromiseError(
+      shoppingListsDatabase.createShoppingList({
+        user_id: new mongoose.Types.ObjectId(userId),
+        name: shoppingListInfo.name,
+        description: shoppingListInfo.description,
+        products: [],
+      })
+    );
+    if (error) {
+      throw error;
+    }
+
+    return {
+      id: shoppingList._id.toString(),
+      user_id: shoppingList.user_id.toString(),
+      name: shoppingList.name,
+      description: shoppingList.description,
+      products: [],
+    };
   },
   updateShoppingList: async (
     id: string,
     userId: string,
-    shoppingListInfo: Omit<ShoppingListDto, "id" | "products" | "user_id">
+    shoppingListInfo: Partial<
+      Omit<ShoppingListDto, "id" | "products" | "user_id">
+    >
   ): Promise<ShoppingListDto> => {
     return {} as ShoppingListDto;
   },
@@ -29,15 +117,15 @@ const shoppingListsService = {
     id: string,
     userId: string,
     productId: string
-  ): Promise<{ product: ProductDto; amount: number }> => {
-    return { product: {} as ProductDto, amount: 0 };
+  ): Promise<{ product: ProductDto; amount: number }[]> => {
+    return [{ product: {} as ProductDto, amount: 0 }];
   },
   removeProductFromShoppingList: async (
     id: string,
     userId: string,
     productId: string
-  ): Promise<{ product: ProductDto; amount: number }> => {
-    return { product: {} as ProductDto, amount: 0 };
+  ): Promise<{ product: ProductDto; amount: number }[]> => {
+    return [{ product: {} as ProductDto, amount: 0 }];
   },
 };
 
